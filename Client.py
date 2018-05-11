e8faa12ac6b55eff6426e4d1d3dc1219c9e682b7
bigfocusbear <37442772+bigfocusbear@users.noreply.github.com>
2018-05-11 15:13:09 +0800

    Update Client.py
    input name part

--- Client.py
+++ Client.py
@@ -6,7 +6,10 @@ class Client:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock = sock
         self.sock.connect((host, port))
+        print('Input your name:')#chenda
+        name = input()
         self.sock.send(b'1')
+        self.sock.send(name.encode())
 
     def sendThreadFunc(self):
         print("Welcome to chat room!") #by Wei
