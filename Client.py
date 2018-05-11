769218e6a70122b9d04792c26acfacb0a32d879e
vbnmzxc9513 <32253326+vbnmzxc9513@users.noreply.github.com>
2018-05-11 15:53:41 +0800

    Update Client.py

--- Client.py
+++ Client.py
@@ -13,15 +13,17 @@ class Client:
         print ("Chats, Lets Welcome " + str(name)+" join us !!")#by chenpo
         self.sock.send(b'1')
         self.sock.send(name.encode())
+        print(name + ":")
 
     def sendThreadFunc(self):
         global name
-        
+
         while True:
             try:
                 time_str = datetime.datetime.now()
                 myword =  name +":" + input() +"   "+str(time_str.hour)+":"+str(time_str.minute)+":"+str(time_str.second)
                 self.sock.send(myword.encode())
+                print(name + ":")
             except ConnectionAbortedError:
                 print('Server closed this connection!')
             except ConnectionResetError:
